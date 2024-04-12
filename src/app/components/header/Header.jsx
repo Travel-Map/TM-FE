be660422e8ba43c7f4e0770252322f4228fd477e
honi31 <winner020531@naver.com>
2024-04-12 19:29:59 +0900

    [FEAT]-홈 화면과 구분선 추가

--- src/app/components/header/Header.jsx
+++ src/app/components/header/Header.jsx
@@ -2,13 +2,16 @@ import Icon from "/public/header/icon.svg";
 
 export default function Header() {
   return (
-    <div className="w-auto flex justify-between items-center mx-20 my-4">
-      <div className="flex text-3xl text-blue-600 font-bold items-center mr-2">
-        <Icon width="24" height="24" className="font-bold" />
-        TravelMap
-      </div>
+    <>
+      <div className="w-auto flex justify-between items-center mx-20 my-8">
+        <div className="flex text-3xl text-blue-600 font-bold items-center mr-2">
+          <Icon width="24" height="24" className="font-bold" />
+          TravelMap
+        </div>
 
-      <div>profile</div>
-    </div>
+        <div>profile</div>
+      </div>
+      <hr />
+    </>
   );
 }
